d0abed209858328ec64bafa21249f31aab869b32
jamin <jaminwebdev@gmail.com>
2026-01-21 16:08:41 -0600

    cors setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,11 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV,
+  WHITELIST_ORIGINS: [
+    "",
+    // example: 'https://docs.blog-api.codewithjamin.com/'
+  ],
 };
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
 import express from "express";
 import config from "@/config";
 import cors from "cors";
+import type { CorsOptions } from "cors";
 
 const app = express();
 
-app.use(cors());
+const corsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if (
+      config.NODE_ENV === "development" ||
+      !origin ||
+      config.WHITELIST_ORIGINS.includes(origin)
+    ) {
+      callback(null, true);
+    } else {
+      // Reject requests fom non whitelisted origins
+      const message = `CORS Error: ${origin} is not allowed`;
+      callback(new Error(message), false);
+      console.log(message);
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.json({
