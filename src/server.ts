cf45ede37860ca74a7e4c777416deeba4ca7fa67
jamin <jaminwebdev@gmail.com>
2026-01-21 15:37:12 -0600

    ts-config alias created

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
-import config from "./config";
+import config from "@/config";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Howdy partner",
+  });
+});
 
 app.listen(config.PORT, () => {
   console.log(`Server listening on port ${config.PORT}`);
